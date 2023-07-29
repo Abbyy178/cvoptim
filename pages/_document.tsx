b9eeab8d0e21427910c5a60c133e758aeef584df
Abbyy178 <133275193+Abbyy178@users.noreply.github.com>
2023-07-29 12:01:21 +0800

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" href="/ailvsuo1.ico" />
           <meta
             name="description"
             content="Simplify your chat content in seconds."
